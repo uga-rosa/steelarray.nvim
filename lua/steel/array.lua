28c034fd1db805810b9b6a363e03062cb3f42e88
uga-rosa <uga6603@gmail.com>
2021-10-29 07:48:57 +0900

    indent 4

--- lua/steel/array.lua
+++ lua/steel/array.lua
@@ -2,7 +2,7 @@
 local Array = {}
 
 local function is_table(t)
-  assert(type(t) == "table", "t must be table")
+    assert(type(t) == "table", "t must be table")
 end
 
 ---Copies an array recursively.
@@ -10,73 +10,73 @@ end
 ---@param t T[]
 ---@return T[]
 function Array.copy(t)
-  if type(t) ~= "table" then
-    return t
-  end
-  local res = {}
-  for i = 1, #t do
-    res[i] = Array.copy(t[i])
-  end
-  return setmetatable(res, getmetatable(t))
+    if type(t) ~= "table" then
+        return t
+    end
+    local res = {}
+    for i = 1, #t do
+        res[i] = Array.copy(t[i])
+    end
+    return setmetatable(res, getmetatable(t))
 end
 
 ---Copies a table recursively.
 ---@param t table
 ---@return table
 local function tbl_copy(t)
-  if type(t) ~= "table" then
-    return t
-  end
-  local res = {}
-  for k, v in pairs(t) do
-    res[k] = tbl_copy(v)
-  end
-  return setmetatable(res, getmetatable(t))
+    if type(t) ~= "table" then
+        return t
+    end
+    local res = {}
+    for k, v in pairs(t) do
+        res[k] = tbl_copy(v)
+    end
+    return setmetatable(res, getmetatable(t))
 end
 
 ---Checks if t is an array.
 ---@param t table
 ---@return boolean
 function Array.is_array(t)
-  if type(t) ~= "table" then
-    return false
-  end
-  local _t = tbl_copy(t)
-  for i = 1, #t do
-    if _t[i] == nil then
-      return false
+    if type(t) ~= "table" then
+        return false
     end
-    _t[i] = nil
-  end
-  if next(_t) then
-    return false
-  end
-  return true
+    local _t = tbl_copy(t)
+    for i = 1, #t do
+        if _t[i] == nil then
+            return false
+        end
+        _t[i] = nil
+    end
+    if next(_t) then
+        return false
+    end
+    return true
 end
 
 ---Recursively checks if t is an array.
 ---@param t table
 ---@return boolean
 function Array.is_array_deep(t)
-  local function _is_array(t2)
-    for i = 1, #t2 do
-      if t2[i] == nil then
-        return false
-      elseif type(t2[i]) == "table" then
-        if not _is_array(t2[i]) then
-          return false
+    local function _is_array(t2)
+        for i = 1, #t2 do
+            if t2[i] == nil then
+                return false
+            elseif type(t2[i]) == "table" then
+                if not _is_array(t2[i]) then
+                    return false
+                end
+            end
+            t2[i] = nil
         end
-      end
-      t2[i] = nil
+        if next(t2) then
+            return false
+        end
+        return true
     end
-    if next(t2) then
-      return false
-    end
-    return true
-  end
 
-  local _t = tbl_copy(t)
-  return _is_array(_t)
+    local _t = tbl_copy(t)
+    return _is_array(_t)
 end
 
 ---Returns a new instance of Array.
@@ -85,13 +85,13 @@ end
 ---@param check? '"shallow"' | '"deep"'
 ---@return T[] Array
 function Array.new(t, check)
-  is_table(t)
-  if check == "shallow" then
-    assert(Array.is_array(t), "This is not an array-like table.")
-  elseif check == "deep" then
-    assert(Array.is_array_deep(t), "This is not an array-like table.")
-  end
-  return setmetatable(t, { __index = Array })
+    is_table(t)
+    if check == "shallow" then
+        assert(Array.is_array(t), "This is not an array-like table.")
+    elseif check == "deep" then
+        assert(Array.is_array_deep(t), "This is not an array-like table.")
+    end
+    return setmetatable(t, { __index = Array })
 end
 
 ---Returns an array of step (default: 1) increments from first to last.
@@ -100,15 +100,15 @@ end
 ---@param step? integer
 ---@return integer[]
 function Array.range(first, last, step)
-  if type(first) ~= "number" or type(last) ~= "number" then
-    error("first and last must be number")
-  end
-  step = step or 1
-  local t = {}
-  for i = first, last, step do
-    t[#t + 1] = i
-  end
-  return Array.new(t)
+    if type(first) ~= "number" or type(last) ~= "number" then
+        error("first and last must be number")
+    end
+    step = step or 1
+    local t = {}
+    for i = first, last, step do
+        t[#t + 1] = i
+    end
+    return Array.new(t)
 end
 
 ---Returns a new array with the element e repeated n times.
@@ -117,11 +117,11 @@ end
 ---@param n number
 ---@return T[] Array
 function Array.repeats(e, n)
-  local res = {}
-  for i = 1, n do
-    res[i] = Array.copy(e)
-  end
-  return Array.new(res)
+    local res = {}
+    for i = 1, n do
+        res[i] = Array.copy(e)
+    end
+    return Array.new(res)
 end
 
 ---Returns a new array with the elements of t repeated n times.
@@ -130,32 +130,32 @@ end
 ---@param n number
 ---@return T[] Array
 function Array.cycle(t, n)
-  local res = {}
-  local len = #t
-  local c = 0
-  for _ = 1, n do
-    for j = 1, len do
-      c = c + 1
-      res[c] = Array.copy(t[j])
+    local res = {}
+    local len = #t
+    local c = 0
+    for _ = 1, n do
+        for j = 1, len do
+            c = c + 1
+            res[c] = Array.copy(t[j])
+        end
     end
-  end
-  return Array.new(res)
+    return Array.new(res)
 end
 
 ---Returns the array combined multiple arrays.
 ---@vararg any[]
 ---@return any[] Array
 function Array.concat(...)
-  local res = {}
-  local c = 0
-  local args = { ... }
-  for i = 1, #args do
-    for j = 1, #args[i] do
-      c = c + 1
-      res[c] = Array.copy(args[i][j])
+    local res = {}
+    local c = 0
+    local args = { ... }
+    for i = 1, #args do
+        for j = 1, #args[i] do
+            c = c + 1
+            res[c] = Array.copy(args[i][j])
+        end
     end
-  end
-  return Array.new(res)
+    return Array.new(res)
 end
 
 ----- Array method from here. -----
@@ -166,11 +166,11 @@ end
 ---@param func fun(a: T1): T2
 ---@return Array #T2[]
 function Array:map(func)
-  local res = {}
-  for i = 1, #self do
-    res[i] = func(self[i])
-  end
-  return Array.new(res)
+    local res = {}
+    for i = 1, #self do
+        res[i] = func(self[i])
+    end
+    return Array.new(res)
 end
 
 ---Returns the Array with all the elements of t that fullfilled the func.
@@ -178,40 +178,40 @@ end
 ---@param func fun(a: any): boolean
 ---@return Array
 function Array:filter(func)
-  local res = {}
-  local c = 0
-  for i = 1, #self do
-    if func(self[i]) then
-      c = c + 1
-      res[c] = Array.copy(self[i])
+    local res = {}
+    local c = 0
+    for i = 1, #self do
+        if func(self[i]) then
+            c = c + 1
+            res[c] = Array.copy(self[i])
+        end
     end
-  end
-  return Array.new(res)
+    return Array.new(res)
 end
 
 local function in_range(t, first, last)
-  is_table(t)
-  if type(first) ~= "number" then
-    error("The argument first must be a number, but " .. type(first))
-  elseif first == 0 then
-    error("The argument first should not be zero")
-  elseif first < 0 then
-    first = #t + first + 1
-  elseif first > #t then
-    error("The argument first is grater than t's length: " .. first .. " > " .. #t)
-  end
-  last = last or #t
-  if type(last) ~= "number" then
-    error("The argument last must be a number, but " .. type(last))
-  elseif last == 0 then
-    error("The argument last should not be zero")
-  elseif last < 0 then
-    last = #t + last + 1
-  elseif last > #t then
-    error("The argument last is grater than t's length: " .. last .. " > " .. #t)
-  end
-  assert(first <= last, "first is grater than last: " .. first .. " > " .. last)
-  return first, last
+    is_table(t)
+    if type(first) ~= "number" then
+        error("The argument first must be a number, but " .. type(first))
+    elseif first == 0 then
+        error("The argument first should not be zero")
+    elseif first < 0 then
+        first = #t + first + 1
+    elseif first > #t then
+        error("The argument first is grater than t's length: " .. first .. " > " .. #t)
+    end
+    last = last or #t
+    if type(last) ~= "number" then
+        error("The argument last must be a number, but " .. type(last))
+    elseif last == 0 then
+        error("The argument last should not be zero")
+    elseif last < 0 then
+        last = #t + last + 1
+    elseif last > #t then
+        error("The argument last is grater than t's length: " .. last .. " > " .. #t)
+    end
+    assert(first <= last, "first is grater than last: " .. first .. " > " .. last)
+    return first, last
 end
 
 ---Deletes the elements of the array t at positions `first..last`
@@ -220,16 +220,16 @@ end
 ---@param last integer
 ---@return Array
 function Array:delete(first, last)
-  first, last = in_range(self, first, last)
-  local res = {}
-  local c = 0
-  for i = 1, #self do
-    if i < first or i > last then
-      c = c + 1
-      res[c] = self[i]
+    first, last = in_range(self, first, last)
+    local res = {}
+    local c = 0
+    for i = 1, #self do
+        if i < first or i > last then
+            c = c + 1
+            res[c] = self[i]
+        end
     end
-  end
-  return Array.new(res)
+    return Array.new(res)
 end
 
 ---Returns the array with the elements inserted from src into t at position pos.
@@ -238,19 +238,19 @@ end
 ---@param pos? integer
 ---@return Array
 function Array:insert(src, pos)
-  src = type(src) == "table" and src or { src }
-  pos = pos or #self
-  local res = {}
-  for i = 1, pos - 1 do
-    res[i] = self[i]
-  end
-  for i = 1, #src do
-    res[pos - 1 + i] = src[i]
-  end
-  for i = pos, #self do
-    res[#src + i] = self[i]
-  end
-  return Array.new(res)
+    src = type(src) == "table" and src or { src }
+    pos = pos or #self
+    local res = {}
+    for i = 1, pos - 1 do
+        res[i] = self[i]
+    end
+    for i = 1, #src do
+        res[pos - 1 + i] = src[i]
+    end
+    for i = pos, #self do
+        res[#src + i] = self[i]
+    end
+    return Array.new(res)
 end
 
 ---Returns the slice of the array t.
@@ -260,12 +260,12 @@ end
 ---@param last? integer
 ---@return Array
 function Array:slice(first, last)
-  first, last = in_range(self, first, last)
-  local res = {}
-  for i = first, last do
-    res[#res + 1] = self[i]
-  end
-  return Array.new(res)
+    first, last = in_range(self, first, last)
+    local res = {}
+    for i = first, last do
+        res[#res + 1] = self[i]
+    end
+    return Array.new(res)
 end
 
 ---Checks if t contains e.
@@ -273,13 +273,13 @@ end
 ---@param e any
 ---@return boolean
 function Array.contain(self, e)
-  is_table(self)
-  for i = 1, #self do
-    if e == self[i] then
-      return true
+    is_table(self)
+    for i = 1, #self do
+        if e == self[i] then
+            return true
+        end
     end
-  end
-  return false
+    return false
 end
 
 ---Returns how many e's are contained in the array t.
@@ -287,14 +287,14 @@ end
 ---@param e any
 ---@return integer
 function Array:count(e)
-  is_table(self)
-  local res = 0
-  for i = 1, #self do
-    if e == self[i] then
-      res = res + 1
+    is_table(self)
+    local res = 0
+    for i = 1, #self do
+        if e == self[i] then
+            res = res + 1
+        end
     end
-  end
-  return res
+    return res
 end
 
 ---Checks if any element of t fullfilled func.
@@ -302,13 +302,13 @@ end
 ---@param func fun(x: any): boolean
 ---@return boolean
 function Array:any(func)
-  is_table(self)
-  for i = 1, #self do
-    if func(self[i]) then
-      return true
+    is_table(self)
+    for i = 1, #self do
+        if func(self[i]) then
+            return true
+        end
     end
-  end
-  return false
+    return false
 end
 
 ---Checks if all the elements of t fullfilled func.
@@ -316,29 +316,29 @@ end
 ---@param func fun(x: any): boolean
 ---@return boolean
 function Array:all(func)
-  is_table(self)
-  for i = 1, #self do
-    if not func(self[i]) then
-      return false
+    is_table(self)
+    for i = 1, #self do
+        if not func(self[i]) then
+            return false
+        end
     end
-  end
-  return true
+    return true
 end
 
 ---Returns the array without duplicates.
 ---@param self Array
 ---@return Array
 function Array:deduplicate()
-  is_table(self)
-  local res = {}
-  local c = 0
-  for i = 1, #self do
-    if not Array.contain(res, self[i]) then
-      c = c + 1
-      res[c] = Array.copy(self[i])
+    is_table(self)
+    local res = {}
+    local c = 0
+    for i = 1, #self do
+        if not Array.contain(res, self[i]) then
+            c = c + 1
+            res[c] = Array.copy(self[i])
+        end
     end
-  end
-  return Array.new(res)
+    return Array.new(res)
 end
 
 ---Returns the copy of the sorted array t.
@@ -347,29 +347,29 @@ end
 ---@param cmp? fun(x: T, y: T): boolean #default: `<`
 ---@return Array
 function Array:sort(cmp)
-  is_table(self)
-  local res = Array.copy(self)
-  table.sort(res, cmp)
-  return res
+    is_table(self)
+    local res = Array.copy(self)
+    table.sort(res, cmp)
+    return res
 end
 
 ---Flattens the array t
 ---@param self Array
 ---@return Array
 function Array:flatten()
-  is_table(self)
-  local res = {}
-  local function _flatten(arr)
-    for i = 1, #arr do
-      if type(arr[i]) == "table" then
-        _flatten(arr[i])
-      else
-        table.insert(res, arr[i])
-      end
+    is_table(self)
+    local res = {}
+    local function _flatten(arr)
+        for i = 1, #arr do
+            if type(arr[i]) == "table" then
+                _flatten(arr[i])
+            else
+                table.insert(res, arr[i])
+            end
+        end
     end
-  end
-  _flatten(self)
-  return Array.new(res)
+    _flatten(self)
+    return Array.new(res)
 end
 
 ---Returns the array with a combination of t1 and t1.
@@ -379,14 +379,14 @@ end
 ---@param t2 Array T2[]
 ---@return Array {x: T1, y: T2}[]
 function Array:zip(t2)
-  is_table(self)
-  is_table(t2)
-  local res = {}
-  local len = #self < #t2 and #self or #t2
-  for i = 1, len do
-    res[i] = { self[i], t2[i] }
-  end
-  return Array.new(res)
+    is_table(self)
+    is_table(t2)
+    local res = {}
+    local len = #self < #t2 and #self or #t2
+    for i = 1, len do
+        res[i] = { self[i], t2[i] }
+    end
+    return Array.new(res)
 end
 
 ---Unzipping the array of the array with two elements and returns each.
@@ -394,36 +394,36 @@ end
 ---@param self Array #{x: T1, y: T2}[]
 ---@return T1[] Array, T2[] Array
 function Array:unzip()
-  is_table(self)
-  local res1, res2 = {}, {}
-  for i = 1, #self do
-    res1[i] = self[i][1]
-    res2[i] = self[i][2]
-  end
-  return Array.new(res1), Array.new(res2)
+    is_table(self)
+    local res1, res2 = {}, {}
+    for i = 1, #self do
+        res1[i] = self[i][1]
+        res2[i] = self[i][2]
+    end
+    return Array.new(res1), Array.new(res2)
 end
 
 ---Reverses the content of the array t.
 ---@param self Array
 ---@return Array
 function Array:reverse()
-  is_table(self)
-  local i, n = 1, #self
-  while i < n do
-    self[i], self[n] = self[n], self[i]
-    i = i + 1
-    n = n - 1
-  end
-  return self
+    is_table(self)
+    local i, n = 1, #self
+    while i < n do
+        self[i], self[n] = self[n], self[i]
+        i = i + 1
+        n = n - 1
+    end
+    return self
 end
 
 ---Returns the reverse of the array t.
 ---@param self Array
 ---@return Array
 function Array:reversed()
-  is_table(self)
-  local res = Array.copy(self)
-  return Array.reverse(res)
+    is_table(self)
+    local res = Array.copy(self)
+    return Array.reverse(res)
 end
 
 ---Returns the result of left convolution.
@@ -433,20 +433,20 @@ end
 ---@param first? T
 ---@return T
 function Array:foldl(func, first)
-  is_table(self)
-  assert(#self > 0, "Can't fold empty array")
-  local res, start
-  if first then
-    res = first
-    start = 1
-  else
-    res = self[1]
-    start = 2
-  end
-  for i = start, #self do
-    res = func(res, self[i])
-  end
-  return res
+    is_table(self)
+    assert(#self > 0, "Can't fold empty array")
+    local res, start
+    if first then
+        res = first
+        start = 1
+    else
+        res = self[1]
+        start = 2
+    end
+    for i = start, #self do
+        res = func(res, self[i])
+    end
+    return res
 end
 
 ---Returns the result of right convolution.
@@ -456,20 +456,20 @@ end
 ---@param first? T
 ---@return T
 function Array:foldr(func, first)
-  is_table(self)
-  assert(#self > 0, "Can't fold empty array")
-  local res, start
-  if first then
-    res = first
-    start = #self
-  else
-    res = self[#self]
-    start = #self - 1
-  end
-  for i = start, 1, -1 do
-    res = func(res, self[i])
-  end
-  return res
+    is_table(self)
+    assert(#self > 0, "Can't fold empty array")
+    local res, start
+    if first then
+        res = first
+        start = #self
+    else
+        res = self[#self]
+        start = #self - 1
+    end
+    for i = start, 1, -1 do
+        res = func(res, self[i])
+    end
+    return res
 end
 
 return Array
--- lua/steel/vector.lua
+++ lua/steel/vector.lua
@@ -2,38 +2,38 @@
 local vector = {}
 
 local function is_table(t)
-  return type(t) == "table"
+    return type(t) == "table"
 end
 
 local function tbl_copy(t)
-  if not is_table(t) then
-    return t
-  end
-  local res = {}
-  for k, v in pairs(t) do
-    res[k] = tbl_copy(v)
-  end
-  return setmetatable(res, getmetatable(t))
+    if not is_table(t) then
+        return t
+    end
+    local res = {}
+    for k, v in pairs(t) do
+        res[k] = tbl_copy(v)
+    end
+    return setmetatable(res, getmetatable(t))
 end
 
 ---Checks if t is a vector.
 ---@param t table
 ---@return boolean, string
 function vector.is_vector(t)
-  if not is_table(t) then
-    return false
-  end
-  local _t = tbl_copy(t)
-  for i = 1, #t do
-    if _t[i] == nil or type(_t[i]) ~= "number" then
-      return false
+    if not is_table(t) then
+        return false
     end
-    _t[i] = nil
-  end
-  if next(_t) then
-    return false
-  end
-  return true
+    local _t = tbl_copy(t)
+    for i = 1, #t do
+        if _t[i] == nil or type(_t[i]) ~= "number" then
+            return false
+        end
+        _t[i] = nil
+    end
+    if next(_t) then
+        return false
+    end
+    return true
 end
 
 ---Initializes a vector.
@@ -41,87 +41,87 @@ end
 ---@param init number
 ---@return Vector
 function vector.init(num, init)
-  init = init or 0
-  local res = {}
-  for i = 1, num do
-    res[i] = init
-  end
-  return res
+    init = init or 0
+    local res = {}
+    for i = 1, num do
+        res[i] = init
+    end
+    return res
 end
 
 ---Copies the vector.
 ---@param vec Vector
 ---@return Vector
 function vector.copy(vec)
-  local res = {}
-  for i = 1, #vec do
-    res[i] = vec[i]
-  end
-  return res
+    local res = {}
+    for i = 1, #vec do
+        res[i] = vec[i]
+    end
+    return res
 end
 
 ---Finds the maximum value of the vector.
 ---@param self Vector
 ---@return number
 function vector:max()
-  local res = self[1]
-  for i = 2, #self do
-    if self[i] > res then
-      res = self[i]
+    local res = self[1]
+    for i = 2, #self do
+        if self[i] > res then
+            res = self[i]
+        end
     end
-  end
-  return res
+    return res
 end
 
 ---Finds the minimum value of the vector.
 ---@param self Vector
 ---@return number
 function vector:min()
-  local res = self[1]
-  for i = 2, #self do
-    if self[i] < res then
-      res = self[i]
+    local res = self[1]
+    for i = 2, #self do
+        if self[i] < res then
+            res = self[i]
+        end
     end
-  end
-  return res
+    return res
 end
 
 ---Finds the sum of the vector.
 ---@param self Vector
 ---@return number
 function vector:sum()
-  local res = self[1]
-  for i = 2, #self do
-    res = res + self[i]
-  end
-  return res
+    local res = self[1]
+    for i = 2, #self do
+        res = res + self[i]
+    end
+    return res
 end
 
 ---Finds the mean of the vector.
 ---@param self Vector
 ---@return number
 function vector:mean()
-  return vector.sum(self) / #self
+    return vector.sum(self) / #self
 end
 
 ---Finds the variance of the vector.
 ---@param self Vector
 ---@return number
 function vector:variance(unbiased)
-  local n = unbiased and #self - 1 or #self
-  local mean = vector.mean(self)
-  local res = 0
-  for i = 1, #self do
-    res = res + (self[i] - mean) ^ 2
-  end
-  return res / n
+    local n = unbiased and #self - 1 or #self
+    local mean = vector.mean(self)
+    local res = 0
+    for i = 1, #self do
+        res = res + (self[i] - mean) ^ 2
+    end
+    return res / n
 end
 
 ---Finds the standard deviation of the vector.
 ---@param self Vector
 ---@return number
 function vector:standard_deviation(unbiased)
-  return math.sqrt(vector.variance(self, unbiased))
+    return math.sqrt(vector.variance(self, unbiased))
 end
 
 ---Creates the histogram of the vector.
@@ -132,37 +132,37 @@ end
 ---@return Vector histogram
 ---@return Vector bin_edges
 function vector:histogram(bins, range, density)
-  local vec = vector.copy(self)
-  table.sort(vec)
-  bins = bins or 10
-  range = range or { vec[1], vec[#vec] }
-  density = density or false
+    local vec = vector.copy(self)
+    table.sort(vec)
+    bins = bins or 10
+    range = range or { vec[1], vec[#vec] }
+    density = density or false
 
-  local width = (range[2] - range[1]) / bins
-  local bin_edges = { [1] = range[1], [bins + 1] = range[2] }
-  for i = 2, bins do
-    bin_edges[i] = range[1] + width * (i - 1)
-  end
-
-  local hist = vector.init(bins)
-  local i, j = 1, 1
-  while j < bins do
-    if vec[i] < bin_edges[j + 1] then
-      hist[j] = hist[j] + 1
-      i = i + 1
-    else
-      j = j + 1
+    local width = (range[2] - range[1]) / bins
+    local bin_edges = { [1] = range[1], [bins + 1] = range[2] }
+    for i = 2, bins do
+        bin_edges[i] = range[1] + width * (i - 1)
     end
-  end
-  hist[bins] = #vec - i + 1
 
-  if density then
-    for k = 1, #hist do
-      hist[k] = hist[k] / #vec
+    local hist = vector.init(bins)
+    local i, j = 1, 1
+    while j < bins do
+        if vec[i] < bin_edges[j + 1] then
+            hist[j] = hist[j] + 1
+            i = i + 1
+        else
+            j = j + 1
+        end
     end
-  end
+    hist[bins] = #vec - i + 1
 
-  return hist, bin_edges
+    if density then
+        for k = 1, #hist do
+            hist[k] = hist[k] / #vec
+        end
+    end
+
+    return hist, bin_edges
 end
 
 ---Some kind of calculation.
@@ -172,30 +172,30 @@ end
 ---@overload fun(a: number, b:Vector): Vector
 ---@overload fun(a: Vector, b:number): Vector
 local function vec_calc(a, b, cal)
-  local typeA = type(a)
-  local typeB = type(b)
-  local res = {}
-  if typeA == "table" and typeB == "table" then
-    assert(#a == #b, ("attempt to calculate between vectors of different sizes (%s and %s)"):format(#a, #b))
-    for i = 1, #a do
-      res[i] = cal(a[i] - b[i])
+    local typeA = type(a)
+    local typeB = type(b)
+    local res = {}
+    if typeA == "table" and typeB == "table" then
+        assert(#a == #b, ("attempt to calculate between vectors of different sizes (%s and %s)"):format(#a, #b))
+        for i = 1, #a do
+            res[i] = cal(a[i] - b[i])
+        end
+    elseif typeA == "number" and typeB == "table" then
+        for i = 1, #b do
+            res[i] = cal(a - b[i])
+        end
+    elseif typeA == "table" and typeB == "number" then
+        for i = 1, #a do
+            res[i] = cal(a[i] - b)
+        end
+    else
+        assert(false, ("attempt to calculate between %s and %s"):format(typeA, typeB))
     end
-  elseif typeA == "number" and typeB == "table" then
-    for i = 1, #b do
-      res[i] = cal(a - b[i])
-    end
-  elseif typeA == "table" and typeB == "number" then
-    for i = 1, #a do
-      res[i] = cal(a[i] - b)
-    end
-  else
-    assert(false, ("attempt to calculate between %s and %s"):format(typeA, typeB))
-  end
-  return res
+    return res
 end
 
 local function add(a, b)
-  return a + b
+    return a + b
 end
 
 ---Addition
@@ -205,11 +205,11 @@ end
 ---@overload fun(a: number, b:Vector): Vector
 ---@overload fun(a: Vector, b:number): Vector
 function vector.add(a, b)
-  return vec_calc(a, b, add)
+    return vec_calc(a, b, add)
 end
 
 local function sub(a, b)
-  return a - b
+    return a - b
 end
 
 ---Subtraction
@@ -219,11 +219,11 @@ end
 ---@overload fun(a: number, b:Vector): Vector
 ---@overload fun(a: Vector, b:number): Vector
 function vector.sub(a, b)
-  return vec_calc(a, b, sub)
+    return vec_calc(a, b, sub)
 end
 
 local function mul(a, b)
-  return a * b
+    return a * b
 end
 
 ---Multiplication
@@ -233,11 +233,11 @@ end
 ---@overload fun(a: number, b:Vector): Vector
 ---@overload fun(a: Vector, b:number): Vector
 function vector.mul(a, b)
-  return vec_calc(a, b, mul)
+    return vec_calc(a, b, mul)
 end
 
 local function div(a, b)
-  return a / b
+    return a / b
 end
 
 ---Division
@@ -247,7 +247,7 @@ end
 ---@overload fun(a: number, b:Vector): Vector
 ---@overload fun(a: Vector, b:number): Vector
 function vector.div(a, b)
-  return vec_calc(a, b, div)
+    return vec_calc(a, b, div)
 end
 
 ---inner product
@@ -255,21 +255,21 @@ end
 ---@param b Vector
 ---@return number
 function vector.prod(a, b)
-  if type(a) ~= "table" then
-    error("a must be table")
-  end
-  if type(b) ~= "table" then
-    error("b must be table")
-  end
-  if #a == #b then
-    local res = 0
-    for i = 1, #a do
-      res = res + a[i] * b[i]
+    if type(a) ~= "table" then
+        error("a must be table")
     end
-    return res
-  else
-    error("#a must be equal to #b")
-  end
+    if type(b) ~= "table" then
+        error("b must be table")
+    end
+    if #a == #b then
+        local res = 0
+        for i = 1, #a do
+            res = res + a[i] * b[i]
+        end
+        return res
+    else
+        error("#a must be equal to #b")
+    end
 end
 
 return vector
